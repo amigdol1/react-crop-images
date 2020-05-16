d03f9ff98b80dba24b5f0464cec635795e63e0af
Alex Migdol <amigdol@cvent.com>
2020-05-16 15:24:40 -0700

    add styles and update to do something with file onload

--- src/components/drop-zone.js
+++ src/components/drop-zone.js
@@ -1,15 +1,31 @@
 // drop-zone docs: https://www.npmjs.com/package/react-dropzone
-
-import React from 'react'
+import React, {useCallback} from 'react'
 import {useDropzone} from 'react-dropzone'
+import '../styles.css'
 
 function ImageDropZone() {
-  const {getRootProps, getInputProps} = useDropzone()
+
+  const onDrop = useCallback((acceptedFiles) => {
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader()
+
+      reader.onabort = () => console.log('file reading was aborted')
+      reader.onerror = () => console.log('file reading has failed')
+      reader.onload = () => {
+      // Do whatever you want with the file contents
+        const binaryStr = reader.result
+        console.log(binaryStr)
+      }
+      reader.readAsArrayBuffer(file)
+    })
+
+  }, [])
+  const {getRootProps, getInputProps} = useDropzone({onDrop})
 
   return (
-    <div {...getRootProps()}>
+    <div className="drop-zone" {...getRootProps()}>
       <input {...getInputProps()} />
-      <p>Select your image</p>
+      <p>Drag Files Here!</p>
     </div>
   )
 }
