9828bbdb5cbc714c62c0c114d40cee03bea3c42a
Alex Migdol <amigdol@cvent.com>
2020-05-17 11:23:51 -0700

    add state to files and preview image after upload

--- src/components/drop-zone.js
+++ src/components/drop-zone.js
@@ -1,31 +1,42 @@
 // drop-zone docs: https://www.npmjs.com/package/react-dropzone
-import React, {useCallback} from 'react'
+import React, {useCallback, useState} from 'react'
 import {useDropzone} from 'react-dropzone'
 import '../styles.css'
 
+// have accepted file types
+
 function ImageDropZone() {
+  const [imgSrc, setImgSrc] = useState(null);
 
   const onDrop = useCallback((acceptedFiles) => {
+    console.log(acceptedFiles);
     acceptedFiles.forEach((file) => {
       const reader = new FileReader()
 
       reader.onabort = () => console.log('file reading was aborted')
       reader.onerror = () => console.log('file reading has failed')
       reader.onload = () => {
-      // Do whatever you want with the file contents
-        const binaryStr = reader.result
-        console.log(binaryStr)
+        // update state of image so it displays
+        setImgSrc(reader.result)
       }
-      reader.readAsArrayBuffer(file)
+      reader.readAsDataURL(file)
     })
 
   }, [])
-  const {getRootProps, getInputProps} = useDropzone({onDrop})
+
+  const handleSubmit = (file) => {
+    // check if the user cropped
+    // Email image on submit
+    // potentially send to server
+  }
+
+  const {getRootProps, getInputProps} = useDropzone({onDrop});
 
   return (
     <div className="drop-zone" {...getRootProps()}>
       <input {...getInputProps()} />
       <p>Drag Files Here!</p>
+      {imgSrc !== null ? <img src={imgSrc} alt='file upload preview' /> : ''}
     </div>
   )
 }
